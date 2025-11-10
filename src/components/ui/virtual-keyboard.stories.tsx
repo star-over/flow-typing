6c154f24df8a1f8bf272d78fa48fcd2b5fe6e283
FFire <8497715@gmail.com>
2025-11-10 15:30:27 +0500

    fix: SB

--- src/components/ui/virtual-keyboard.stories.tsx
+++ src/components/ui/virtual-keyboard.stories.tsx
@@ -1,41 +1,45 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 
-
 import { KeyboardLayoutANSI } from "@/data/keyboard-layout-ansi";
 import { fingerLayoutASDF } from "@/data/finger-layout-asdf";
 import { symbolLayoutEnQwerty } from "@/data/symbol-layout-en-qwerty";
-import { VirtualKeyboard } from "./virtual-keyboard";
+import { VirtualKeyboard, type VirtualKeyboardProps } from "./virtual-keyboard";
 import { createVirtualLayout } from "@/lib/virtual-layout";
 
-const meta = {
-  component: VirtualKeyboard,
-} satisfies Meta<typeof VirtualKeyboard>;
+type StoryArgs = VirtualKeyboardProps & { shift: boolean };
+
+const meta: Meta<StoryArgs> = {
+  // Утверждение типа `as any` здесь - это осознанный шаг,
+  // чтобы разрешить конфликт между расширенными StoryArgs и реальными пропами компонента.
+  component: VirtualKeyboard as any,
+  argTypes: {
+    shift: {
+      name: 'Shift Modifier',
+      control: 'boolean',
+      description: 'Toggle to switch between lower and upper case',
+    },
+    virtualLayout: {
+      control: false,
+    },
+  },
+};
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<StoryArgs>;
 
-const virtualLayout = createVirtualLayout({
-  keyboardLayout: KeyboardLayoutANSI,
-  symbolLayout: symbolLayoutEnQwerty,
-  fingerLayout: fingerLayoutASDF,
-});
-
-const virtualLayoutCaps = createVirtualLayout({
-  keyboardLayout: KeyboardLayoutANSI,
-  symbolLayout: symbolLayoutEnQwerty,
-  fingerLayout: fingerLayoutASDF,
-  shift: true,
-})
-
-export const LowerCase: Story = {
+export const Interactive: Story = {
   args: {
-    virtualLayout,
-  }
-};
+    shift: false,
+  },
+  render: ({ shift }) => {
+    const virtualLayout = createVirtualLayout({
+      keyboardLayout: KeyboardLayoutANSI,
+      symbolLayout: symbolLayoutEnQwerty,
+      fingerLayout: fingerLayoutASDF,
+      shift: shift,
+    });
 
-export const UpperCase: Story = {
-  args: {
-    virtualLayout: virtualLayoutCaps,
-  }
+    return <VirtualKeyboard  {...{ virtualLayout }} />;
+  },
 };
