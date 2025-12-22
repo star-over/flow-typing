386bba40311b23e4e3fd2f48825067481e554be3
FFire <8497715@gmail.com>
2025-12-22 23:20:33 +0700

    chore(deps): update dependencies and adjust vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,8 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+// import { playwright } from '@vitest/browser-playwright';
+// import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const dirname =
@@ -24,23 +24,23 @@ export default defineConfig({
           },
         },
       },
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybookTest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright(),
-          },
-          setupFiles: ['.storybook/vitest.setup.ts'],
-        },
-      },
+      // {
+      //   extends: true,
+      //   plugins: [
+      //     // The plugin will run tests for the stories defined in your Storybook config
+      //     // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybookTest
+      //     storybookTest({ configDir: path.join(dirname, '.storybook') }),
+      //   ],
+      //   test: {
+      //     name: 'storybook',
+      //     browser: {
+      //       enabled: true,
+      //       headless: true,
+      //       provider: playwright(),
+      //     },
+      //     setupFiles: ['.storybook/vitest.setup.ts'],
+      //   },
+      // },
     ],
   },
 });
